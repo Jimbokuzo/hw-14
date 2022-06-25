fb3de2e540ab969d6b225edbc70c153819bd1e66
Jimbokuzo <yegorgrunsky@gmail.com>
2022-06-25 20:33:46 +0300

    11 commit

--- src/features/header/components/MenuNavigation.tsx
+++ src/features/header/components/MenuNavigation.tsx
@@ -7,7 +7,7 @@ export const MenuNavigation = () => (
   <ul
     css={css`
       display: flex;
-      justify-content: space-around;
+      justify-content: space-evenly;
       align-items: center;
       width: 100%;
       height: 50px;
---
+++ src/features/home/components/AddPostPopup.tsx
@@ -0,0 +1,55 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import AddIcon from "@mui/icons-material/Add";
+import { AddPostForm } from "./AddPostForm";
+
+interface Props {}
+
+export const AddPostPopup = (props: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        onClick={handleClickOpen}
+        endIcon={<AddIcon />}
+      >
+        Add post
+      </Button>
+      <Dialog
+        open={open}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle
+          css={css`
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            cursor: default;
+          `}
+        >
+          Add post
+        </DialogTitle>
+        <DialogContent>
+          <AddPostForm handleClose={handleClose} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
